e8540ba7b05998ad12648167ee94ca2ce7de49ab
Ulisses <ulissessuporteti01@gmail.com>
2021-11-22 22:13:16 -0300

    Revert "Revert "Primeiro laço for para gravar os dados na matriz""
    This reverts commit e8f9e883f809b2e99c010b4eb180bffa7b375afe.

--- aula100Matrizes/src/application/Program.java
+++ aula100Matrizes/src/application/Program.java
@@ -11,7 +11,14 @@ public class Program {
 		int m = sc.nextInt();
 
 		int[][] mat = new int[n][m];
-
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		
+		
 	}
 
 }
