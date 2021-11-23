20eba22eb6a80b9a6cd5829aa2e64730f40e4ce5
Ulisses <ulissessuporteti01@gmail.com>
2021-11-22 21:56:13 -0300

    Criado a matriz com o tamenho definido pelo o usuário.

--- aula100Matrizes/src/application/Program.java
+++ aula100Matrizes/src/application/Program.java
@@ -1,9 +1,16 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+
+		int[][] mat = new int[n][m];
 
 	}
 
