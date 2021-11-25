77e2c77c24278cabc0903df67d65d7755e048d10
Ulisses <ulissessuporteti01@gmail.com>
2021-11-24 22:10:59 -0300

    Criado a variável x o laço for e o if's para a validação

--- aula100Matrizes/src/application/Program.java
+++ aula100Matrizes/src/application/Program.java
@@ -11,14 +11,28 @@ public class Program {
 		int m = sc.nextInt();
 
 		int[][] mat = new int[n][m];
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
 				mat[i][j] = sc.nextInt();
 			}
 		}
-		
-		
+		int x = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (mat[i][j] == x) {// verifica e valor da matriz da match com o x.
+					System.out.println("Position: " + i + "," + j);//Posição
+					if ((j < m) && (j >= 0))
+						System.out.println("Left: " + mat[i][j - 1]);//validação da posição a esquerda
+					if (j < n)
+						System.out.println("Right: " + mat[i][j + j]);//validação da posição a direita
+					if (i > 0)
+						System.out.println("Up: " + mat[i - 1][j]);//validação da posição acima
+					if (i < n)
+						System.out.println("Down: " + mat[i + 1][j]);//validação da posição abaixo
+				}
+			}
+		}
+		sc.close();
 	}
-
 }
